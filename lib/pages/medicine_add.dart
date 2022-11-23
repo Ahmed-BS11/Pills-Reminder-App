96997cfa737232d6d3b3eef1cd6a98c049f3bed5
sarra.hammami <sarra.hammami456@gmail.com>
2022-11-23 13:01:15 +0100

    second

--- lib/pages/medicine_add.dart
+++ lib/pages/medicine_add.dart
@@ -2,8 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widgets/pagefirst.dart';
 import 'package:flutter_application_1/theme.dart';
+
+  //const Med_add({super.key});
+
+
 class Med_add extends StatelessWidget {
-  const Med_add({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
@@ -26,12 +30,83 @@ class Med_add extends StatelessWidget {
           ),
           
         ),
-        containerChild:Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-      )
+        containerChild:
+        Padding(
+          padding: EdgeInsets.fromLTRB(MediaQuery. of(context). size. width / 20,0, MediaQuery. of(context). size. width / 20,0 ),
+          child: Column(
+            children:<Widget>[
+              Container(
+                child:Text('shape',textAlign:TextAlign.left,
+                style:TextStyle(
+                  fontSize:15,
+                  fontFamily: 'Raleway',
+                  color:Colors.black54,
+                  fontWeight: FontWeight.w200,
+                ),
+                
+                ),
+
+              ),
+              SizedBox(height: 10,),
+              Container(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child:Row(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                            Image.asset('assets/medicineBottle (1).png',
+                            width:150,
+                            height:150,),
+                            Image.asset('assets/im1.png',
+                            width:150,
+                            height:150,),
+                            Image.asset('assets/pill.png',
+                            width:150,
+                            height:150,),
+                            Image.asset('assets/im4.jpg',
+                            width:150,
+                            height:150,)
+                          ],
+           )
+          ),
+              ),
+               SizedBox( width: 10),
+              Container(
+                        child:Row(mainAxisAlignment:MainAxisAlignment.spaceEvenly ,
+                           children: [
+                             Container(
+                               child: TextField(
+                                decoration:InputDecoration(
+                                  filled:true,
+                                  fillColor:Colors.white,
+                                  hintText:'Name',
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: BorderSide.none,
+                                )
+                                  ),
+                               
+          ),
+                             ),
+                          
+                        ],)
+                     //SizedBox( width: 10),
+              
+          
+                )
+               
+            ]
+            
+          
+             
+          ),
+        )
+       
+
 
         
       );
     
   }
+ 
 }
